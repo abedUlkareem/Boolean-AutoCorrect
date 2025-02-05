8aeb810432c900e52ec66318a77164defdca9daf
abedUlkareem <158261918+abedUlkareem@users.noreply.github.com>
2025-02-05 18:04:35 +0300

    Add files via upload

---
+++ mylibrary/stopword1.py
@@ -0,0 +1,111 @@
+def arabic():
+    arabic_path = r"D:\code of university\stopword_28 language\arabic.txt"
+    return arabic_path
+
+def bulgarian():
+    bulgarian_path = r"D:\code of university\stopword_28 language\bulgarian.txt"
+    return bulgarian_path
+
+def catalan():
+    catalan_path = r"D:\code of university\stopword_28 language\catalan.txt"
+    return catalan_path
+
+def czech():
+    czech_path = r"D:\code of university\stopword_28 language\czech.txt"
+    return czech_path
+
+def danish():
+    danish_path = r"D:\code of university\stopword_28 language\danish.txt"
+    return danish_path
+
+def dutch():
+    dutch_path = r"D:\code of university\stopword_28 language\dutch.txt"
+    return dutch_path
+
+def english():
+    english_path = r"D:\code of university\stopword_28 language\english.txt"
+    return english_path
+
+def finnish():
+    finnish_path = r"D:\code of university\stopword_28 language\finnish.txt"
+    return finnish_path
+
+def french():
+    french_path = r"D:\code of university\stopword_28 language\french.txt"
+    return french_path
+
+def german():
+    german_path = r"D:\code of university\stopword_28 language\german.txt"
+    return german_path
+
+def gujarati():
+    gujarati_path = r"D:\code of university\stopword_28 language\gujarati.txt"
+    return gujarati_path
+
+def hebrew():
+    hebrew_path = r"D:\code of university\stopword_28 language\hebrew.txt"
+    return hebrew_path
+
+def hindi():
+    hindi_path = r"D:\code of university\stopword_28 language\hindi.txt"
+    return hindi_path
+
+def hungarian():
+    hungarian_path = r"D:\code of university\stopword_28 language\hungarian.txt"
+    return hungarian_path
+
+def indonesian():
+    indonesian_path = r"D:\code of university\stopword_28 language\indonesian.txt"
+    return indonesian_path
+
+def italian():
+    italian_path = r"D:\code of university\stopword_28 language\italian.txt"
+    return italian_path
+
+def malaysian():
+    malaysian_path = r"D:\code of university\stopword_28 language\malaysian.txt"
+    return malaysian_path
+
+def norwegian():
+    norwegian_path = r"D:\code of university\stopword_28 language\norwegian.txt"
+    return norwegian_path
+
+def polish():
+    polish_path = r"D:\code of university\stopword_28 language\polish.txt"
+    return polish_path
+
+def portuguese():
+    portuguese_path = r"D:\code of university\stopword_28 language\portuguese.txt"
+    return portuguese_path
+
+def romanian():
+    romanian_path = r"D:\code of university\stopword_28 language\romanian.txt"
+    return romanian_path
+
+def russian():
+    russian_path = r"D:\code of university\stopword_28 language\russian.txt"
+    return russian_path
+
+def slovak():
+    slovak_path = r"D:\code of university\stopword_28 language\slovak.txt"
+    return slovak_path
+
+def spanish():
+    spanish_path = r"D:\code of university\stopword_28 language\spanish.txt"
+    return spanish_path
+
+def swedish():
+    swedish_path = r"D:\code of university\stopword_28 language\swedish.txt"
+    return swedish_path
+
+def turkish():
+    turkish_path = r"D:\code of university\stopword_28 language\turkish.txt"
+    return turkish_path
+
+def ukrainian():
+    ukrainian_path = r"D:\code of university\stopword_28 language\ukrainian.txt"
+    return ukrainian_path
+
+def vietnamese():
+    vietnamese_path = r"D:\code of university\stopword_28 language\vietnamese.txt"
+    return vietnamese_path
